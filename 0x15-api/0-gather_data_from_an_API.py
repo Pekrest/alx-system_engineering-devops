188208da7dfc6fe53f8690d8dad523a2d93ec18c
pekrest <peteroku3@gmail.com>
2024-05-20 15:58:06 -0700

    Gather data from an API for me

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,18 +9,18 @@ from requests import get
 from sys import argv
 
 if __name__ == '__main__':
-    base_url = 'https://jsonplaceholder.typicode.com'
-    todos_url = base_url + "/users/{}/todos".format(argv[1])
-    user_url = base_url + "/users/{}".format(argv[1])
-    todos_result = get(todos_url).json()
-    name_result = get(user_url).json()
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    todos_num = len(todos_result)
-    todos_complete = len([todos for todos in todos_result
-                         if todos.get("completed")])
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
     name = name_result.get("name")
     print("Employee {} is done with tasks({}/{}):"
-          .format(name, todos_complete, todos_num))
-    for todos in todos_result:
-        if (todos.get("completed")):
-            print("\t {}".format(todos.get("title")))
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
